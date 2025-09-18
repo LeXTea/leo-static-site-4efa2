b3a4120222f9010ef926874d4fc4923ea78dcde7
LeQ <47932446+LeXTea@users.noreply.github.com>
2025-09-17 20:17:24 -0700

    Test update on front page

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hi, I'm Leo Quach" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <a href="https://www.farcaster.xyz/">
+            Visit my Farcaster!
+          </a>
         </p>
       </main>
 
