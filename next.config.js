68b1107c9c1223a4f7b43ffe7ac648758e57b9d6
LeQ <47932446+LeXTea@users.noreply.github.com>
2025-09-24 22:39:38 -0700

    update manifest temp redirect

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01997f34-333a-3988-8da1-fda071145554',
+        permanent: false,
+      },
+    ]
+  },
+}
