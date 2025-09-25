caf4cf41d521a87d814000cfc42001fa5c96cb63
LeQ <47932446+LeXTea@users.noreply.github.com>
2025-09-25 10:40:11 -0700

    removed unnecessary react import

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import { sdk } from '@farcaster/miniapp-sdk'
-import { useEffect } from 'react'
 
 export default function Home() {
   <script type="module">
